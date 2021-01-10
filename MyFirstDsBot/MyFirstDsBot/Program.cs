3fb1873edd2b600a105cf9d530904e2e89e757d4
Tikhon-ql <tihkon.grek@mail.ru>
2021-01-10 16:40:04 +0300

    commit

---
+++ MyFirstDsBot/MyFirstDsBot/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyFirstDsBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+        private DiscordSocketClient discordClient;
+        private CommandService commandService;
+        private IServiceProvider services;
+        public async Task RunBotAsync()
+        {
+            discordClient = new DiscordSocketClient();
+            commandService = new CommandService();
+            services = new ServiceCollection()
+                .AddSingleton(discordClient)
+                .AddSingleton(commandService)
+                .BuildServiceProvider();
+
+            string token = "Nzk3MjE2ODA1NjkyMTEyOTE4.X_jQAg.OJ83SZKqKFIXqMtsWk2IWCRbV_U";
+
+            discordClient.Log += DiscordClient_Log;
+
+            await RegisterCommandsAsync();
+
+            await discordClient.LoginAsync(TokenType.Bot, token);
+            await discordClient.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task DiscordClient_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            discordClient.MessageReceived += HandleCommandAsync;
+            await commandService.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage message = (SocketUserMessage)arg;
+            SocketCommandContext context = new SocketCommandContext(discordClient, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                IResult result = await commandService.ExecuteAsync(context, argPos, services);
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
---
+++ MyFirstDsBot/MyFirstDsBot/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("MyFirstDsBot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyFirstDsBot")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("86a9a9f1-8e4b-4b89-aba2-c87a1d9f8fa8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
